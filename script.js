f04adca0bd8c7775279d8c07f758865064b21f97
ColeAT <49347013+CucumberToes@users.noreply.github.com>
2024-06-16 17:07:50 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,19 +5,3 @@ document.getElementById("myBtn").addEventListener("click", function() {
     var modal = document.getElementById("myModal");
     modal.style.display = modal.style.display === "none" ? "block" : "none";
 });
-
-if (document.getElementById("questionForm")) {
-    document.getElementById("questionForm").onsubmit = function(event) {
-        event.preventDefault(); 
-        var userAnswer = document.getElementById("animalTerms").value.toLowerCase();
-        var responseText = '';
-
-        if (userAnswer === "cheetah") {
-            responseText = "Correct!";
-        } else {
-            responseText = "Incorrect, try again.";
-        }
-
-        document.getElementById("response").textContent = responseText;
-    };
-}
